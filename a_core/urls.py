b59c1b418633e10f74c32ffb6929bededbd18751
BIN BIN <thangpd300502@gmail.com>
2024-08-27 11:41:38 +0700

    UPLOAD FILE

--- a_core/urls.py
+++ a_core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from a_posts.views import *
@@ -12,3 +14,5 @@ urlpatterns = [
     path("post/edit/<pk>/", post_edit_view, name="post-edit"),
     path("post/<pk>/", post_page_view, name="post"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
